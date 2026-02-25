dae9914a682ca1cedced363f09c1b83dff4021ef
Daniel Spors <daniels@scavix.com>
2026-02-25 08:52:20 +0100

    PHP 8.5 compat: Moved from "__sleep/__wakeup" to "__serialize/__unserialize", Removed non-canonical cast names

--- src/cssmin.php
+++ src/cssmin.php
@@ -2327,7 +2327,7 @@ class CssMin
      */
     public static function setVerbose($to)
     {
-        self::$isVerbose = (boolean) $to;
+        self::$isVerbose = (bool) $to;
         return self::$isVerbose;
     }
     /**
